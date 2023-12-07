6a01cd5237283197e7028f949e2dab913cce0448
phwGithub <yey6689@gmail.com>
2023-12-07 18:27:15 +0900

    JDBC 개발 - 조회

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,26 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        // save
         Member memberV0 = new Member("memberV0", 10000);
         repositoryV0.save(memberV0);
+
+        // findById
+        Member findMember = repositoryV0.findById(memberV0.getMemberId());
+        log.info("findMember={}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(memberV0); // = 비교는 false, equals 비교는 true
     }
 }

beda6dc463a75b8243b7d53db31aa774fa088ccd
phwGithub <yey6689@gmail.com>
2023-12-14 23:09:55 +0900

    트랜잭션 문제해결 - 트랜잭션 템플릿

--- jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
+++ jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -1,7 +1,6 @@
 package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV2;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
@@ -19,23 +18,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - 트랜잭션 템플릿
  */
 @Slf4j
-public class MemberServiceV3_1Test {
+public class MemberServiceV3_2Test {
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
 
     private MemberRepositoryV3 memberRepository;
-    private MemberServiceV3_1 memberService;
+    private MemberServiceV3_2 memberService;
 
     @BeforeEach
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
         memberRepository = new MemberRepositoryV3(dataSource);
-        memberService = new MemberServiceV3_1(memberRepository, transactionManager);
+        memberService = new MemberServiceV3_2(memberRepository, transactionManager);
     }
 
     @AfterEach
